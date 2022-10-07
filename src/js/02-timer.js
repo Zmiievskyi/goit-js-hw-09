20bf982a25b6e6d9f6d658b89a22b8cedc5c0949
Anton <104930896+Zmiievskyi@users.noreply.github.com>
2022-10-07 13:30:42 +0100

    task 2

---
+++ src/js/02-timer.js
@@ -0,0 +1,69 @@
+import flatpickr from 'flatpickr';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import 'flatpickr/dist/flatpickr.min.css';
+
+const options = {
+  altInput: true,
+  altFormat: 'F j, Y',
+  dateFormat: 'Y-m-d',
+  //   minDate: ,
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose,
+};
+
+const refs = {
+  startBtn: document.querySelector('.btn'),
+  days: document.querySelector('span[data-days]'),
+  hours: document.querySelector('span[data-hours]'),
+  min: document.querySelector('span[data-minutes]'),
+  sec: document.querySelector('span[data-seconds]'),
+};
+
+flatpickr('#datetime-picker', options);
+
+refs.startBtn.disabled = true;
+
+function onClose(selectedDates) {
+  const actualData = Date.now();
+
+  if (selectedDates[0].getTime() >= actualData) {
+    refs.startBtn.disabled = false;
+    refs.startBtn.addEventListener('click', () => {
+      refs.startBtn.disabled = true;
+      setInterval(() => {
+        const timer = getTimeComponents(
+          selectedDates[0].getTime() - Date.now()
+        );
+        const { days, hours, mins, secs } = timer;
+        const dayLeft = addLeadingZero(days);
+        const hoursLeft = addLeadingZero(hours);
+        const minutesLeft = addLeadingZero(mins);
+        const secondsLeft = addLeadingZero(secs);
+        refs.days.textContent = dayLeft;
+        refs.hours.textContent = hoursLeft;
+        refs.min.textContent = minutesLeft;
+        refs.sec.textContent = secondsLeft;
+      }, 1000);
+    });
+  } else {
+    Notify.failure('Please choose a data in tha future');
+  }
+}
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
+
+function getTimeComponents(time) {
+  const days = Math.floor(
+    (time % (1000 * 60 * 60 * 24 * 365)) / (1000 * 60 * 60 * 24)
+  );
+  const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+  const secs = Math.floor((time % (1000 * 60)) / 1000);
+
+  return { days, hours, mins, secs };
+}
--- src/js/module.js
+++ src/js/module.js
@@ -1,3 +1,5 @@
+// for task 1-------------------------------------
+
 const refs = {
   startBtn: document.querySelector('button[data-start]'),
   stopBtn: document.querySelector('button[data-stop]'),
@@ -8,4 +10,18 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-export { refs, getRandomHexColor };
+// for task 2-------------------------------------
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+};
+
+
+
+
+export { refs, getRandomHexColor, options };
